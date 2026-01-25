3978152923b36997690f624ab1021122b0324089
Lukas Bidzisgan <Godadzeluca@gmail.com>
2026-01-25 15:53:17 +0400

    submitted a homework #1

--- #1.py
+++ #1.py
@@ -1,1 +1,20 @@
-#hello
+# 1
+
+with open("first numbers.txt", "r", encoding="utf-8") as f1, \
+     open("second numbers.txt", "w", encoding="utf-8") as f2:
+
+    for line in f1:
+        f1_pow = int(line) ** 2
+        print(f1_pow)
+        f2.write(str(f1_pow) + "\n")
+
+print("--------------------")
+
+#2
+
+insert_a_year = input("Enter a year: ")
+
+with open("oscars.txt", "r", encoding="utf-8") as f3:
+    for line in f3:
+        if insert_a_year in line:
+            print(line)
