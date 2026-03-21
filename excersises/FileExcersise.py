3dd8a2085fcf729c060e6bf9f71d0c6ff7e5c26b
ydwagz <godadzeluca@gmail.com>
2026-03-21 10:58:06 +0400

    <>

--- excersises/FileExcersise.py
+++
@@ -1,9 +0,0 @@
-with open("FileExcersise.txt", "w") as f1:
-    f1.write("Hello world!" + '\n' + "hello")
-    f1.close()
-
-with open("FileExcersise.txt", "r") as f1:
-    print(f1.read())
-    for char in f1:
-        data = char.strip().split(" ")
-        print(data)
